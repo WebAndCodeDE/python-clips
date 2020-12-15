1b00aedac3e8e7ac5b263aa6af8b8d25b4f4407f
Andy <lockdown20@outlook.de>
2020-12-15 11:23:00 +0100

    ...

--- temp_file.py
+++ temp_file.py
@@ -1,16 +1,12 @@
 my_dict = {
-    "Tisch" : "table",
-    "Stuhl" : "chair"
+    "Tisch": "table",
+    "Stuhl": "chair"
 }
 
 a = input().split()
-if len(a)>1:
-    x,y = a
+if len(a) > 1:
+    x, y = a
     print(x)
     print(y)
-    my_dict[x]=y
-    print(my_dict)
-
-if len(a)<2:
-    my_dict[a[0]]()
+    my_dict[x] = y
     print(my_dict)
